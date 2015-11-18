4f5ec3373c87c6f23191d964e73abfa46786fb68
Baptiste Fontaine <batifon@yahoo.fr>
2015-11-18 13:42:13 +0100

    brew-for-each-formula: don't run if ARGV.empty?

--- cmd/brew-for-each-formula.rb
+++ cmd/brew-for-each-formula.rb
@@ -9,5 +9,10 @@
 # copied from https://github.com/jacknagel/dotfiles/blob/master/bin/brew-for-each-formula.rb
 #
 
+if ARGV.empty?
+  puts "Usage:\n\tbrew for-each-formula <ruby code>"
+  exit 1
+end
+
 require "formula"
 Formula.each { |f| eval(ARGV.first) }
