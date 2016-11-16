7d35cefd56d63f301140a63e47a8572590e76b3f
Tim D. Smith <github@tim-smith.us>
2016-11-15 23:05:29 -0800

    Add brew-resource-versions (#9)
    * Add brew-resource-versions
    
    Useful for discovering out-of-date or insecure versions of vendored
    dependencies.
    
    * Use `brew help`-style comments

---
+++ cmd/brew-resource-versions.rb
@@ -0,0 +1,17 @@
+#:  * `resource-versions` <resource name>:
+#: Print versions of resources with the given name in all formulas
+#: Example:
+#:   brew resource-versions cryptography
+
+if ARGV.empty?
+  puts "Usage:\n\tbrew resource-versions <resource name>"
+  exit 1
+end
+
+require "formula"
+package = ARGV.first
+Formula.
+  select { |f| f.resources.any? { |r| r.name == package } }.
+  map { |f| [f.resources.select { |r| r.name == package }.first.version, f.full_name] }.
+  sort.
+  each { |version, name| puts "#{name},#{version}" }
